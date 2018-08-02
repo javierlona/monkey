0f480effaa3ffbc2592dc745b47ed07e4f62f12e
Javier Lona <javierlona1@gmail.com>
2018-08-02 15:33:35 -0500

    navbar colors set

--- public/index.php
+++ public/index.php
@@ -11,13 +11,13 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-md">
+    <nav class="navbar navbar-expand-lg navbar-dark">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#monkediaNav" aria-controls="monkediaNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <div class="collapse navbar-collapse" id="monkediaNav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -25,7 +25,6 @@
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
           </li>
-        </ul>
       </div>
     </nav>
 
